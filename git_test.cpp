619291650b158dd252816013c370d94ac446b600
huyendieu909 <huyendieu909@github.com>
2025-07-17 09:24:33 +0700

    2ndcommit

--- git_test.cpp
+++ git_test.cpp
@@ -4,5 +4,6 @@ using namespace std;
 int main() {
     cout << "Hello, World!" << endl;
     cout << "This is a test for git." << endl;
+    cout << "2nd commit in 2nd branch." << endl;
     return 0;
 }
